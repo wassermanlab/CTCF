e0990261fa6483cd0a38a3e013e4d419d7a7282b
Oriol Fornés <oriol.fornes@gmail.com>
2020-05-12 14:56:15 -0700

    added bash script to download chromosome X of different species

---
+++ Genomes/X/get_chrX.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+for g in bosTau6 equCab3 gorGor5 hg38 mm10 oviAri3 panPan2 panTro6 ponAbe3
+do
+    if [ ! -f ${g}.chrX.fa ]; then
+        mkdir ${g}
+        cd ${g}
+        wget ftp://hgdownload.soe.ucsc.edu/goldenPath/${g}/bigZips/${g}.fa.gz
+        gunzip ${g}.fa.gz
+        csplit -s -z ${g}.fa '/>/' '{*}'
+        for i in xx* ; do \
+            n=$(sed 's/>// ; s/ .*// ; 1q' "$i") ; \
+            mv "$i" "$n.fa" ; \
+        done
+        cd ..
+        mv ${g}/chrX.fa ${g}.chrX.fa
+        rm -rf ${g}
+    fi
+done
