0333f30b9be0654ce81b376c567eb38d5bee9507
oriolfornes <oriol.fornes@gmail.com>
2020-07-24 14:26:57 -0700

    added CTCF DragoNN models

--- DragoNN/dragonn.sh
+++ DragoNN/dragonn.sh
@@ -2,6 +2,6 @@
 
 # Train a CTCF model with DragoNN
 # https://github.com/kundajelab/dragonn
-dragonn train --pos-sequences ../sequences/pos_seqs.fa --neg-sequences ../sequences/neg_seqs.fa --prefix CTCF
-dragonn train --pos-sequences ../sequences/pos_seqs.fwd.fa --neg-sequences ../sequences/neg_seqs.fwd.fa --prefix CTCF-fwd
-dragonn train --pos-sequences ../sequences/pos_seqs.rev.fa --neg-sequences ../sequences/neg_seqs.rev.fa --prefix CTCF-rev
+dragonn train --pos-sequences ../Data/pos_seqs.fa --neg-sequences ../Data/neg_seqs.fa --prefix CTCF
+dragonn train --pos-sequences ../Data/pos_seqs.fwd.fa --neg-sequences ../Data/neg_seqs.fwd.fa --prefix CTCF-fwd
+dragonn train --pos-sequences ../Data/pos_seqs.rev.fa --neg-sequences ../Data/neg_seqs.rev.fa --prefix CTCF-rev
