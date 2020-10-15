30c296f82ed85f5e958f03601f49bda7deb05f5a
oriolfornes <oriol.fornes@gmail.com>
2020-10-15 08:51:35 -0700

    added GC-matched sequences and CTCF predictions

--- Data/scan-sequence.sh
+++ Data/scan-sequence.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
+
 # Get CTCF predictions in the set of positive CTCF-bound sequences
-python ~/GitHub/JASPAR-UCSC-tracks/scan_sequence.py --fasta-file ./pos_seqs.fa --profiles-dir ~/GitHub/JASPAR-UCSC-tracks/profiles --threads 4 --profile MA0139.1
+python ../JASPAR-UCSC-tracks/scan_sequence.py --fasta-file ./pos_seqs.fa --profiles-dir ../JASPAR-UCSC-tracks/profiles --threads 4 --profile MA0139.1
